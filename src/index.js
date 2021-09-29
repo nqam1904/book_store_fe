ae748edaf39b8c963bc43fff6ee784308996ba44
TaiTran2512 <trantantai25121998@gmail.com>
2021-09-29 16:00:14 +0700

    add redux persist + set up

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import "./index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import configureStore, { history } from "redux/store";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { Loading } from "components";
-import { BrowserRouter } from "react-router-dom";
-let store = configureStore();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import configureStore, { history } from 'redux/store'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Loading } from 'components'
+import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+
+let { store, persistor } = configureStore()
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App history={history} />
-        <ToastContainer
-          position="bottom-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-        <Loading ref={(refs) => Loading.setRef(refs)} />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+	<React.StrictMode>
+		<BrowserRouter>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<App history={history} />
+					<ToastContainer
+						position="bottom-center"
+						autoClose={5000}
+						hideProgressBar={false}
+						newestOnTop={false}
+						closeOnClick
+						rtl={false}
+						pauseOnFocusLoss
+						draggable
+						pauseOnHover
+					/>
+					<Loading ref={(refs) => Loading.setRef(refs)} />
+				</PersistGate>
+			</Provider>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,33 @@
-import { applyMiddleware, createStore } from "@reduxjs/toolkit";
-import createSagaMiddleware from "redux-saga";
-import { rootSaga } from "redux/saga";
-import rootReducer from "redux/reducers";
-import { routerMiddleware } from "connected-react-router";
-import { createBrowserHistory } from "history";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware, createStore } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
+import { rootSaga } from 'redux/saga'
+import rootReducer from 'redux/reducers'
+import { routerMiddleware } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware()
 export const history = createBrowserHistory({
-  forceRefresh: true,
-});
+	forceRefresh: true,
+})
+//config persist storage
+const persistConfig = {
+	key: 'root',
+	storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer(history))
+
+//
 export default function configureStore(preloadedState) {
-  const middlewares = [sagaMiddleware, routerMiddleware(history)];
+	const middlewares = [sagaMiddleware, routerMiddleware(history)]
 
-  const composedEnhancer = composeWithDevTools(applyMiddleware(...middlewares));
+	const composedEnhancer = composeWithDevTools(applyMiddleware(...middlewares))
 
-  const store = createStore(
-    rootReducer(history),
-    preloadedState,
-    composedEnhancer
-  );
-  sagaMiddleware.run(rootSaga);
-  return store;
+	const store = createStore(persistedReducer, preloadedState, composedEnhancer)
+	let persistor = persistStore(store)
+
+	sagaMiddleware.run(rootSaga)
+	return { store, persistor }
 }
