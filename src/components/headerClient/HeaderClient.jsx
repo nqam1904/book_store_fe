a726715108f4b71990457c60f171a052199b28f3
MinhNQA <nghiemminh1904@gmail.com>
2021-10-01 22:45:20 +0700

    button login discord

--- src/components/headerClient/HeaderClient.jsx
+++ src/components/headerClient/HeaderClient.jsx
@@ -39,6 +39,11 @@ const HeaderClient = () => {
 	const onFormLogin = () => {
 		canvasRef.current.open()
 	}
+	const onLoginDiscord = () => {
+		window.open(
+			'https://discord.com/api/oauth2/authorize?client_id=893506542073171968&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code&scope=identify%20email'
+		)
+	}
 	return (
 		<>
 			<header className="header">
@@ -94,6 +99,10 @@ const HeaderClient = () => {
 					/>
 					<Button variant="success" type="submit">
 						Login
+					</Button>
+
+					<Button variant="success" onClick={onLoginDiscord}>
+						Discord
 					</Button>
 				</form>
 			</Canvas>
